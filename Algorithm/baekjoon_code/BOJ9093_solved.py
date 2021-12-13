967e888cf69367f447e0bbe26cda66c2b24694ac
tigre911 <94420939+tigre911@users.noreply.github.com>
2021-12-13 18:12:16 +0900

    Create BOJ9093_solved.py

---
+++ Algorithm/baekjoon_code/BOJ9093_solved.py
@@ -0,0 +1,19 @@
+t = int(input())
+sentence = []
+for i in range(t):
+    sentence.append(input())
+
+print(f'sentence : {sentence}')
+
+print(f'split : {sentence[0].split()}')
+
+print(f'split[::-1] : {sentence[0][::-1].split()}')
+
+for j in range(t):
+    a = sentence[j][::-1].split()
+    print(f'a : {a}')
+    print(f'a[::-1] : {a[::-1]}')
+    print((' ').join(a[::-1]))
+
+# a = sentence[0][::-1].split()
+# print((' ').join(a[::-1]))
